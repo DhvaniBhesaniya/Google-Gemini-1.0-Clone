37ee0a77b1a50c6ab9e43298dccd0516e27e1d3b
dhvanibhesaniya-inventyv <dhvanibhesaniyainventyv@gmail.com>
2024-05-21 14:57:45 +0530

    completed ai funcnalities with input and response

---
+++ gemini-clone/src/context/Context.jsx
@@ -0,0 +1,71 @@
+import { createContext, useState } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  // console.log(input);
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const delayChar = (index, nextChar) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextChar);
+    }, 75 * index); // Adjust the multiplier for faster or slower typing effect
+  };
+
+  const onSent = async () => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    setPrevPrompts(prev=>[...prev,input])
+    const response = await run(input);
+    let responseArray = response.split("**");
+    let newResponse ;
+
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i % 2 !== 0) {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      } else {
+        newResponse += responseArray[i];
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>")
+    // setResultData(newResponse2);
+    // setResultData(response);
+    let newResponceArray  = newResponse2.split(" ");
+    for(let i=0; i<newResponceArray.length; i++){
+       const nextword = newResponceArray[i];
+       delayChar(i,nextword + " ")
+    }
+    setLoading(false);
+    setInput("");
+  };
+  // onSent("what is react")
+  const contextValue = {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    prevPrompts,
+    setPrevPrompts,
+    showResult,
+    setShowResult,
+    loading,
+    setLoading,
+    resultData,
+    setResultData,
+    onSent,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
